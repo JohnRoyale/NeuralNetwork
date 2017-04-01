ad8b349f7155cab984a6bf89584bbbca486c5037
Home\voldelord <r.f.niel@student.rug.nl>
2017-04-01 14:45:19 +0200

    Neuralnet works I will add comments tomorrow on how it works and I'll make improvements if applicable

--- src/Main.java
+++ src/Main.java
@@ -1,123 +1,169 @@
+import java.util.Arrays;
 import java.util.Random;
 
-
 public class Main {
-	static float[][][] weights;
-	static int[] size= {2,2,1};
-	static float[][] activation;
-	static float learningRate = 0.2f;
-	
-	private static float sigmoid(float in){
-		return (float) (1 / Math.exp(-in)+1);
+	static double[][][] weights;
+	static int[] size = { 2, 10, 1 };
+	static double[][] activation;
+	static int printD = 500;
+	static double learningRate = 0.005;
+	static double maxError = 0.01;
+
+	private static double sigmoid(double in) {
+		return (double) (1 / Math.exp(-in) + 1);
 	}
-	private static float dSigmoid(float in){
-		float t=sigmoid(in);
-		return t * (1-t);
+
+	private static double dSigmoid(double in) {
+		double t = sigmoid(in);
+		return t * (1 - t);
 	}
-	private static float[][] arrayTranspose(float [][] A) {
-		float[][] B = new float[][];
-        // transpose
-        if (A.length > 0) {
-            for (int i = 0; i < A[0].length; i++) {
-                for (int j = 0; j < A.length; j++) {
-                    System.out.print(A[j][i] + " ");
-                }
-                System.out.print("\n");
-            }
-        }
-		return B;
+
+	private static double[][] forwardProp() {
+
+		for (int i = 0; i < size.length - 1; i++) {
+			activation[i + 1] = new double[size[i + 1]];
+			for (int j = 0; j < weights[i].length; j++) {
+				for (int k = 0; k < weights[i][j].length; k++) {
+					activation[i + 1][j] += activation[i][k] * weights[i][j][k];
+				}
+				if (i < size.length - 2) {
+					activation[i + 1][j] = sigmoid(activation[i + 1][j]);
+				}
+			}
+		}
+
+		return activation;
 	}
-	
-	private static float[][] arrayMultiplication(float[][] A, float[][] B) {
-		
+
+	private static void arrayAdd(double[] A, double[] B) {
+		if (A.length != B.length)
+			System.err.print("Arrays need to be off equals size to substract");
+
+		for (int i = 0; i < A.length; i++)
+			A[i] = A[i] + B[i];
+	}
+
+	private static double[] arraySub(double[] A, double[] B) {
+		if (A.length != B.length)
+			System.err.print("Arrays need to be off equals size to substract");
+		double[] C = new double[A.length];
+		for (int i = 0; i < A.length; i++)
+			C[i] = A[i] - B[i];
 		return C;
 	}
-	
-	private static float[] forwardProp(float[] input){
-		float[] out=new float[size[size.length-1]];
-		float[] cIn=input.clone();
-		
-		for(int i=0;i<size.length-1;i++){
-			out=new float[size[i+1]];
-			for(int j=0;j<weights[i].length;j++){
-				for(int k=0;k<weights[i][j].length;k++){
-					out[j] +=cIn[k]*weights[i][j][k];	//hidden activation = input activation * hidden weights 
-				}
-				//?doesnt cloning to activation go here?
-				out[j]=sigmoid(out[j]);					//hidden output = sigmoid hidden_activation
-				//out[j]= out[j] > 2 ? 1 : 0;
-			}
-//			for(int j=0;j<out.length;j++) System.out.print(out[j]+" ");
-//			System.out.println();
-			activation[i]=out.clone();					//hidden output aswell??
-			cIn=out.clone();
-		}
-		
-		return out;
-	}
-	
-	private static void backProp(float[][] activation,float[] input,float[] output,float[] expectedOutput){
-		
-		//Walk backwards through layers
-		
-		//hidden activation -> original out[j]
-		//hidden output -> new out[j], now cloned to activation
-		//output activation -> hidden output * weights[1][0][0,1]
-		//output -> outputFunction(output activation) (returns output activation)
-		//output error -> expected output - output
-		//local gradient output -> dsigmoid(output activation) * output error
-		//hidden error -> (local gradient output * weights[1][0][0,1]) and arraytranspose
-		//local gradient hidden -> dsigmoid(original out[j]).*output error (arraymultiplication)
-		//delta output -> learningRate * (hidden output (new out[j]).* local gradient output) (arraymultiplication)
-		//delta hidden -> learningRate * (input .* local gradient hidden) (arraymultiplication)
-		//weights hidden -> weights[0][0,1][0,1] * delta hidden
-		//weight output -> weights[1][0][0,1] * delta output
-		
-		//determine error and gradient for every neuron in layer
-		//error output= output-expected output
-		//local_gradient_output = d_output_function(output_activation)*output_error;
-		//delta_output = learn_rate*hidden_output.*local_gradient_output;
-		//delta_hidden = [examples(pattern,:) bias_value].’*local_gradient_hidden*learn_rate;
-		//% Update the weight matrices
-		//w_hidden = w_hidden+delta_hidden;
-		//w_output = w_output+delta_output.’;
-		System.out.println("output = " + output[0] + " last in activation = " + activation[1][0]);
-		
-	}
-	
-	
-	public static void main(String[] args) {
-		//input=new float[size[0]];
-		float[] input= {0,1};
-		float[] expectedOutput={1};
-		float[] output;
-		weights=new float[size.length-1][][];
-		activation=new float[size.length-1][];
-		
-		for(int i=0;i<weights.length;i++){
-			weights[i]=new float[size[i+1]][size[i]];
-		}
-		Random r=new Random();
-		for(int i=0;i<weights.length;i++){
-			for(int j=0;j<weights[i].length;j++){
-				for(int k=0;k<weights[i][j].length;k++){
-					weights[i][j][k]=r.nextFloat()*2-1;
-					System.out.print(weights[i][j][k]+" ");
-					System.out.print("x");
-				}
-				System.out.print("y");
-				System.out.println();
-			}
-			System.out.print("z");
-			System.out.println();
-		}
-		System.out.println(weights[1][0][1]);
-		float[] test=forwardProp(input); //is test final output really output according to "output = output_function(output_activation);"?
-		
-		backProp(activation, input, test, expectedOutput);
 
-		
-		
+	private static void backProp(double[][] activation, double[] expectedOutput) {
+		double[][] error = new double[activation.length][];
+
+		for (int i = 0; i < activation.length; i++) {
+			error[i] = new double[activation[i].length];
+		}
+
+		double[][] delta = new double[size.length][];
+		for (int i = 0; i < delta.length; i++) {
+			delta[i] = new double[size[i]];
+		}
+
+		error[error.length - 1] = arraySub(expectedOutput, activation[activation.length - 1]);
+		double[][] localGradient = new double[error.length][];
+		for (int i = 0; i < error.length; i++) {
+			localGradient[i] = Arrays.copyOf(error[i], error[i].length);
+		}
+
+		for (int k = weights.length - 1; k > 0; k--) {
+			if (k > 0) {
+				error[k] = new double[error[k].length];
+				localGradient[k] = new double[localGradient[k].length];
+			}
+			for (int i = 0; i < delta[k + 1].length; i++) {
+				if (k != weights.length - 1) {
+					delta[k + 1][i] = error[k + 1][i] * dSigmoid(activation[k + 1][i]);
+				} else {
+					delta[k + 1][i] = error[k + 1][i];
+				}
+				for (int j = 0; j < weights[k][i].length; j++) {
+					error[k][j] += delta[k + 1][i] * weights[k][i][j];
+				}
+			}
+		}
+
+		for (int k = weights.length - 1; k > 0; k--) {
+			for (int i = 0; i < weights[k].length; i++) {
+				for (int j = 0; j < weights[k][i].length; j++) {
+					weights[k][i][j] += learningRate * delta[k + 1][i] * activation[k][j];
+				}
+			}
+		}
+
 	}
-	
+
+	private static double error(double[] output, double[] expectedOutput) {
+		double error = 0;
+		double t;
+		if (output.length != expectedOutput.length)
+			System.err.println("Cannot calculate error if arrays not same size");
+
+		for (int i = 0; i < output.length; i++) {
+			t = (output[i] - expectedOutput[i]);
+			error += t * t;
+		}
+		return error;
+	}
+
+	public static void main(String[] args) {
+		double[][] input = { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } };
+		double[][] expectedOutput = { { 0 }, { 1 }, { 1 }, { 0 } };
+
+		weights = new double[size.length - 1][][];
+
+		for (int i = 0; i < weights.length; i++) {
+			weights[i] = new double[size[i + 1]][size[i]];
+		}
+
+		Random r = new Random(System.currentTimeMillis());
+		for (int i = 0; i < weights.length; i++) {
+			for (int j = 0; j < weights[i].length; j++) {
+				for (int k = 0; k < weights[i][j].length; k++) {
+					weights[i][j][k] = r.nextDouble() * 2 - 1;
+				}
+			}
+		}
+
+		double error;
+		int i = -1;
+		boolean flag = true;
+		while (flag) {
+			i++;
+			flag = false;
+			if (i % printD == 0)
+				System.out.println("Epoch " + i);
+			for (int k = 0; k < input.length; k++) {
+				activation = new double[size.length][];
+				activation[0] = Arrays.copyOf(input[k], input[k].length);
+				activation = forwardProp();
+				backProp(activation, expectedOutput[k]);
+				error = error(activation[activation.length - 1], expectedOutput[k]);
+				if (Math.floorMod(i, printD) == 0) {
+					System.out.print("Pattern " + k + ": " + activation[activation.length - 1][0]);
+					System.out.println(" Squared Error: " + error);
+				}
+				if (error > maxError && i < 20000)
+					flag = true;
+			}
+			if (i % printD == 0)
+				System.out.println();
+		}
+
+		System.out.println("Epoch " + i);
+		for (int k = 0; k < input.length; k++) {
+			activation = new double[size.length][];
+			activation[0] = Arrays.copyOf(input[k], input[k].length);
+			activation = forwardProp();
+			error = error(activation[activation.length - 1], expectedOutput[k]);
+			System.out.print("Pattern " + k + ": " + activation[activation.length - 1][0]);
+			System.out.println(" Squared Error: " + error);
+
+		}
+	}
+
 }
